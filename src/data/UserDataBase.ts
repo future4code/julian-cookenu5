e41d2c944c45c7455489de6144665c510b47a471
Carolina Souza <62221034+carolsouzamuniz@users.noreply.github.com>
2020-07-16 18:27:58 -0300

    correção na indentação

--- src/data/UserDataBase.ts
+++ src/data/UserDataBase.ts
@@ -2,57 +2,70 @@ import { BaseDatabase } from "./BaseDatabase";
 
 export class UserDatabase extends BaseDatabase {
      
-    private static TABLE_NAME = "Cookenu_User"; 
+  private static TABLE_NAME = "Cookenu_User"; 
+  private static TABLE_FOLLOW = "Cookenu_Follow";
   
-    async createUser(
-      id: string,      
-      name: string,
-      email: string,
-      password: string
-    ) {
-      try {
-          await this.getConnection()
-            .insert({
-              id,
-              name,
-              email,
-              password, 
-            })
-            .into(UserDatabase.TABLE_NAME)
+  async createUser(
+    id: string,      
+    name: string,
+    email: string,
+    password: string
+  ) {
+    try {
+      await this.getConnection()
+        .insert({
+          id,
+          name,
+          email,
+          password, 
+        })
+        .into(UserDatabase.TABLE_NAME)
 
-            BaseDatabase.destroyConnection(); 
-            
+        BaseDatabase.destroyConnection(); 
       } catch (error) {
         throw new Error(error.sqlMessage || error.message)
       }
     }
 
-
-    async getByEmail(email: string): Promise<any> {
-
-      try {
-          const result =
-          await this.getConnection()
+  async getByEmail(email: string): Promise<any> {
+    try {
+      const result =
+        await this.getConnection()
           .select("*")
           .from(UserDatabase.TABLE_NAME)
           .where({email});
           
           return result[0];
 
-      } catch (err) {
-          throw new Error(err.sqlMessage || err.message)
-      }
-
-    public async getUserById(id: string): Promise<any> {
-      const result = await this.getConnection()
-        .select("*")
-        .from(UserDatabase.TABLE_NAME)
-        .where({ id });
-      return result[0];
-
+    } catch (err) {
+      throw new Error(err.sqlMessage || err.message)
     }
+  }
 
+  public async getUserById(id: string): Promise<any> {
+    const result = await this.getConnection()
+      .select("*")
+      .from(UserDatabase.TABLE_NAME)
+      .where({ id });
+      return result[0];
+  } 
+
+  async insertUserIdThatIFollow (id: string): Promise<any>{
+    try {
+      await this.getConnection()
+        .insert("")
+        .from(UserDatabase.TABLE_NAME)
+        .where({id});
+          
+    } catch (err) {
+      throw new Error(err.sqlMessage || err.message)
+    }
+  }
 }
+
+
+
+
 
 
 
