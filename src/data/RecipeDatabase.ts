f18e32def0c58fded6351cd978bfe91d3f24d309
Melissa Tammy <mel.tammy2@gmail.com>
2020-07-17 15:15:10 -0300

    feed finalizado

--- src/data/RecipeDatabase.ts
+++ src/data/RecipeDatabase.ts
@@ -38,4 +38,13 @@ export class RecipeDatabase extends BaseDatabase {
         return result[0];
     } 
 
+    public getFeed = async (id_user: string): Promise<any> => {
+      const result = await this.getConnection().raw(`
+          SELECT r.title, r.description, r.createdAt, u.name FROM Cookenu_Recipe r
+          JOIN Cookenu_Follow f ON f.userToFollowId = r.id_user AND f.id_user = '${id_user}'
+          JOIN Cookenu_User u ON r.id_user = u.id;
+        `)
+      return result[0]
+    }
+
 }
